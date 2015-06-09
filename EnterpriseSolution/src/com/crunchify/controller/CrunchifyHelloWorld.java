c68b67717cb626cee2ff504275e192a06664955c
Charlie <nabarundeysit@gmail.com>
2015-06-09 22:50:14 +0530

    temp2

--- EnterpriseSolution/src/com/crunchify/controller/CrunchifyHelloWorld.java
+++ EnterpriseSolution/src/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -19,5 +19,6 @@ public class CrunchifyHelloWorld {
 				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
 		return new ModelAndView("welcome", "message", message);
 		//temp
+		//temp2
 	}
 }
